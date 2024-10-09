07677d90d43aa8398167a1681e06640e0280a516
manahanCoded <manahandionell1@gmail.com>
2024-10-09 18:52:48 +0800

    folder upload

--- activity7.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Activity 7: Key-Value Pairs with foreach
-
-$student = ["Name"=>"Alice", "Age" => "20", "Grade"=>"A", "City"=>"Baguio"];
-foreach($student as $key => $information){
-    echo $key . ": $information\n";
-}
-
-?>
