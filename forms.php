4cb1b64f9e6ab90336edeb136ec6e241cad633c6
manahanCoded <manahandionell1@gmail.com>
2024-10-14 20:56:50 +0800

    add styles

--- forms.php
+++ forms.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Input Example</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>PHP Input Handling Example</h1>
